bdf96a879fcdfd962078b192a31be46e6bb56fd8
Greedysky <greedysky@163.com>
2026-03-03 11:06:29 +0800

    style: Code format clean

--- visualblurxraysfactory.h
+++ visualblurxraysfactory.h
@@ -30,7 +30,7 @@
 /*!
  * @author Greedysky <greedysky@163.com>
  */
-class VisualBlurXRaysFactory : public QObject, public VisualFactory
+class VisualBlurXRaysFactory : public QObject, private VisualFactory
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qmmp.qmmp.VisualFactoryInterface.1.0")
